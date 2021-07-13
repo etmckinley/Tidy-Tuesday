1426fdbc50026912473a70e9cbc62acab7aa8283
etmckinley <etmckinley@gmail.com>
2021-07-12 21:57:41 -0500

    add unmask

--- 2021/2021-07-13/scoobydoo.R
+++ 2021/2021-07-13/scoobydoo.R
@@ -10,10 +10,28 @@ captured = scoobydoo %>%
          starts_with("captured")) %>%
   pivot_longer(starts_with("captured")) %>%
   group_by(name) %>%
-  arrange(date_aired) %>%
-  mutate(cumulative = ifelse(value == FALSE, 0, 1),
+  arrange(index) %>%
+  mutate(cumulative = ifelse(value == TRUE, 1, 0),
          cumulative = cumsum(cumulative))
 
 captured %>% 
   ggplot(aes(x=index, y=cumulative))+
+  geom_path(aes(color=name))
+
+unmask = scoobydoo %>%
+  select(index,
+         series_name,
+         season,
+         title,
+         date_aired,
+         starts_with("unmask")) %>%
+  mutate(unmask_other = as.character(unmask_other)) %>% 
+  pivot_longer(starts_with("unmask")) %>%
+  group_by(name) %>%
+  arrange(index) %>%
+  mutate(cumulative = ifelse(value == TRUE, 1, 0),
+         cumulative = cumsum(cumulative))
+
+unmask %>% 
+  ggplot(aes(x=date_aired, y=cumulative))+
   geom_path(aes(color=name))
