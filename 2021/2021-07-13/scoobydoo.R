279df13bc0d73574a56de435ebebd3bbf70f1d40
Eliot <etmckinley@nlgaslm76j4ml7h.attlocal.net>
2021-07-12 21:25:03 -0500

    initial

---
+++ 2021/2021-07-13/scoobydoo.R
@@ -0,0 +1,16 @@
+tuesdata = tidytuesdayR::tt_load('2021-07-13')
+scoobydoo = tuesdata$scoobydoo
+
+captured = scoobydoo %>%
+  select(index,
+         series_name,
+         season,
+         title,
+         date_aired,
+         starts_with("captured")) %>%
+  pivot_longer(starts_with("captured")) %>%
+  group_by(name) %>%
+  arrange(date_aired) %>%
+  mutate(cumulative = ifelse(value == FALSE, 0, 1),
+         cumulative = cumsum(cumulative))
+
